3c877a896cefaff286e78d65d81b1c9ae3719460
jmerc-dev <jelmarmercado@gmail.com>
2025-02-19 15:12:25 +0800

    fixed ui with bootstrap

---
+++ login.js
@@ -0,0 +1,32 @@
+function signIn() {
+    let oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth"
+    let form = document.createElement('form')
+    form.setAttribute('method', 'GET')
+    form.setAttribute('action', oauth2Endpoint)
+
+    let params = {
+      "client_id": "452942834062-um560gq2me5vgm0uvo0bp68gruegukks.apps.googleusercontent.com",
+      "redirect_uri":"https://www.projectmanager.publicvm.com/workplace.html",
+      "response_type":"token",
+      "scope":"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
+      "include_granted_scopes":true,
+      'state':'pass-through-value'
+    }
+
+    for(var p in params) {
+      let input = document.createElement('input')
+      input.setAttribute('type', 'hidden')
+      input.setAttribute('name', p)
+      input.setAttribute('value', params[p])
+      form.appendChild(input)
+    }
+
+    document.body.appendChild(form)
+
+    form.submit()
+}
+
+// Navbar Scrolling effect
+$(window).scroll(function(){
+  $("nav").toggleClass('scrolled', $(this).scrollTop() > 50);
+  });
