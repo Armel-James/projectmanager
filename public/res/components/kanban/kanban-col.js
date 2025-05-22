e7b7e1a521502cc444715583e9ef819ddecb9f9e
Armel <81472365+Armel-James@users.noreply.github.com>
2025-05-22 21:38:01 +0800

    kanban cards updating along with data and phase (#15)
    * kanban cards updating along with data and phase
    
    * Update proj.js
    
    ---------
    
    Co-authored-by: jmerc-dev <jelmarmercado@gmail.com>

--- public/res/components/kanban/kanban-col.js
+++ public/res/components/kanban/kanban-col.js
@@ -1,18 +1,20 @@
 
 function renderKanbanColumn(phaseid, phaseName) {
 
-    
+    const newTaskButtonId = 'btn-addtask-' + phaseid;
 
     return `<div class="kanban-col" id="${phaseid}">
+    
                 <div class="col-category">
                     <div class="phase-name">${phaseName}</div>
                     <button class="phasebtn">
                         <svg class="col-more" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="18" cy="12" r="1.5" transform="rotate(90 18 12)" fill="currentColor"/><circle cx="12" cy="12" r="1.5" transform="rotate(90 12 12)" fill="currentColor"/><circle cx="6" cy="12" r="1.5" transform="rotate(90 6 12)" fill="currentColor"/></svg>
                     </button>
                 </div>
-    <div class="kanban-card-container">                                 
-    </div>
-                <button class="new-task-button">Add New Task</button>
+
+                <div class="kanban-card-container"></div>
+
+                <button class="new-task-button" id=${newTaskButtonId}>Add New Task</button>
             </div>`;
 }
 
